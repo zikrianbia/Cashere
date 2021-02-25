f4fe98ec17241b33bf004abac82b5118e71c3d0c
zikrianbia <zikri4nbia@gmail.com>
2021-02-25 17:00:56 +0700

    add tag

--- controllers/tagController.js
+++ controllers/tagController.js
@@ -1,5 +1,30 @@
-class TagController{
+const {Tag} = require('../models')
 
+class TagController{
+    static readTag(req, res){
+        Tag.findAll()
+            .then((Tag) => {
+                res.send(Tag)
+            })
+            .catch((err) => {
+                res.send(err)
+            })
+    }
+    static formTag(req, res){
+        res.render('formTag')
+    }
+    static addTag(req, res){
+        let dataTag = {
+            name: req.body.name,
+        }
+        Tag.create(dataTag)
+            .then(() => {
+                res.redirect('/tags')
+            })
+            .catch((err) => {
+                res.send(err)
+            })
+    }
 }
 
 module.exports = TagController
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const menuRoute = require('./menuRoute')
+const tag = require('./tagRoute')
 
 router.get('/', (req, res) => {
     res.send('halloooooo')
 })
 
 router.use('/menus', menuRoute)
+router.use('/tags', tag)
+
 
 module.exports = router
